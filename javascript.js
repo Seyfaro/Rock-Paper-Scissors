d19fa4194b7d9415557288a0237259ff06d91817
Seyfaro <edenroy@hotmail.co.uk>
2023-03-07 16:22:43 +1000

    Annotation

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 console.log("hello world")
-const values = ['Rock', 'Paper', 'Scissors']
+const values = ['Rock', 'Paper', 'Scissors'] //Values the computer can choose from 
 function getComputerChoice(choice){
-    const randomIndex = Math.floor(Math.random() * values.length)
-    return values[randomIndex]
+    const randomIndex = Math.floor(Math.random() * values.length) //Math Random picks a number between 0 and 1 (including zero) we multiply this by the number of clues the computer chan choose from to create the correct range. Math Floor then rounds this down to an integer.
+    return values[randomIndex]// This returns a position in the list of values, i.e 1 to 3
 }
-let ComputerChoice = getComputerChoice();
+let ComputerChoice = getComputerChoice(); //Creates a variable using the name and position on the l
 console.log(ComputerChoice)
