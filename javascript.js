312abce99169c5105bdb6e65e7fce7643905ed2c
Seyfaro <edenroy@hotmail.co.uk>
2023-03-08 16:00:39 +1000

    Added 'play 5 rounds and give overall score'

--- javascript.js
+++ javascript.js
@@ -3,20 +3,20 @@ console.log("hello world")
  userScore = 0
  computerScore = 0
 
-const values = ['Rock', 'Paper', 'Scissors'] //Values the computer can choose from 
-function getComputerChoice(choice){
+const values = ['rock', 'paper', 'scissors'] //Values the computer can choose from 
+function getComputerChoice(){
     const randomIndex = Math.floor(Math.random() * values.length) //Math Random picks a number between 0 and 1 (including zero) we multiply this by the number of clues the computer chan choose from to create the correct range. Math Floor then rounds this down to an integer.
     return values[randomIndex]// This returns a position in the list of values, i.e 1 to 3
 }
 ComputerChoice = getComputerChoice(); //Creates a variable using the name and position on the l
-ComputerChoice = ComputerChoice.toLowerCase();
-console.log(ComputerChoice)
 
-PlayerChoice = prompt("Rock, Paper or Scissors?") // Brings up the popup box
-PlayerChoice = PlayerChoice.toLowerCase();
+
     
 
 function playRound(PlayerChoice, ComputerChoice){
+    PlayerChoice = PlayerChoice.toLowerCase();
+    ComputerChoice = ComputerChoice.toLowerCase();
+
     //Comparing player and computer choices to determine winner
     if (PlayerChoice ===  ComputerChoice) {
         return 'Draw!';
@@ -26,11 +26,34 @@ function playRound(PlayerChoice, ComputerChoice){
         (PlayerChoice === 'paper' && ComputerChoice === 'scissors')
     ) {
         computerScore++;
-        return 'You lose! ${ComputerChoice} beats ${PlayerChoice}.';
+        //Remember to use Option Key + tilde ~ to include variables in a string!
+        return `You lose! ${ComputerChoice} beats ${PlayerChoice}.`;
     } else {
         userScore++;
-        return 'You win! ${PlayerChoice} beats ${ComputerChoice}.';
+        return `You win! ${PlayerChoice} beats ${ComputerChoice}.`;
     }
 }
 
-console.log(playRound(PlayerChoice, ComputerChoice));
+
+
+function game() {
+    // 5 rounds of the game
+    for (let i = 0; i < 5;  i++) {
+        let PlayerChoice = prompt("Rock, Paper or Scissors?"); // Brings up the popup box
+        //Plays the round using the getComputerChoice result,which is called instead of being stored as a variable so that it is different each time
+        let result = playRound(PlayerChoice, getComputerChoice());
+
+        console.log(result);
+    } 
+
+    //Winner
+    if (userScore > computerScore) {
+        console.log(`You win! Score: ${userScore} / 5 !`)
+    } else if (userScore < computerScore) {
+        console.log(`You lost! Your score is ${userScore} / 5.`)
+    } else { 
+        console.log('Tie!')
+    }
+}
+
+game();
