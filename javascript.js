5120dc8febe88d151f7f3d784ac25f918cdb4696
Seyfaro <edenroy@hotmail.co.uk>
2023-03-08 13:48:25 +1000

    Finished generate round result

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 console.log("hello world")
+
+const userScore = 0
+const computerScore = 0
+
 const values = ['Rock', 'Paper', 'Scissors'] //Values the computer can choose from 
 function getComputerChoice(choice){
     const randomIndex = Math.floor(Math.random() * values.length) //Math Random picks a number between 0 and 1 (including zero) we multiply this by the number of clues the computer chan choose from to create the correct range. Math Floor then rounds this down to an integer.
@@ -8,6 +12,26 @@ let ComputerChoice = getComputerChoice(); //Creates a variable using the name an
 console.log(ComputerChoice)
 
 let PlayerChoice = prompt("Rock, Paper or Scissors?") // Brings up the popup box
-if (PlayerChoice = null || PlayerChoice == '')
+console.log(PlayerChoice)    
 
-    
+//Comparing player and computer choices to determine winner
+if (PlayerChoice ===  ComputerChoice) {
+    result= 'Draw!'
+} else if (PlayerChoice === 'Rock' && ComputerChoice === 'Paper') {
+    result= 'You lose!'
+} else if (PlayerChoice === 'Scissors' && ComputerChoice === 'Rock') {
+    result = 'You lose!'
+} else if (PlayerChoice === 'Paper' && ComputerChoice === 'Scissors') {
+    result = 'You lose!'
+} else {
+    result = 'You win!'
+}
+
+
+console.log(result);
+
+ function playRound(ComputerChoice, PlayerChoice){
+
+ }
+
+ console.log(playRound(PlayerChoice, ComputerChoice));
